bcfc5d327c223a7017125dc19e2f95c9116e7859
aditisah <sditi.sah1@gmail.com>
2022-06-03 23:01:29 +0530

    assignment updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const UserController= require("../controllers/userController");
 const BookController = require("../controllers/bookController");
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-router.post("/createUser", UserController.createUser  );
-router.get("/getUsersData", UserController.getUsersData);
 
 router.post("/createNewBook", BookController.createNewBook);
 router.get("/getAllBooks", BookController.getAllBooks);
