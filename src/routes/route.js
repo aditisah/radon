2db34d2478fa96d46f068569b8ecd93cd4777980
aditisah <sditi.sah1@gmail.com>
2022-06-08 23:09:07 +0530

    assignment submitted

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,12 +7,4 @@ router.get("/getBooksByAuthorId/:author_id", BookController.getBooksByAuthorId)
 router.get("/getListOfAuthors", BookController.getListOfAuthors)
 
 
-
-// router.post("/createBook", BookController.createBook  )
-// router.get("/bookList", BookController.getBooksList)
-// router.post("/getBooksInYear", BookController.getBooksInYear)
-// router.post("/getParticularBooks", BookController.getParticularBooks)
-// router.get("/getXINRBooks", BookController.getXINRBooks)
-// router.get("/getRandomBooks", BookController.getRandomBooks)
-
 module.exports = router;
