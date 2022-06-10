28399656eac7717c340a7ce018b05d74187a6e0e
aditisah <sditi.sah1@gmail.com>
2022-06-10 21:33:14 +0530

    middleware assignment submitted

--- src/index.js
+++ src/index.js
@@ -15,12 +15,6 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
   app.use(
     function(req,res,next){
         let date = moment().format('YYYY-MM-DD h:mm:ss')
