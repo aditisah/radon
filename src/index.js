70b948a961678e1c2e7134481d2266168ad4567e
aditisah <sditi.sah1@gmail.com>
2022-06-10 21:13:11 +0530

    assignment submitted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+let ip = require("ip");
+const moment = require('moment');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,12 +15,22 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
+  app.use(
+    function(req,res,next){
+        let date = moment().format('YYYY-MM-DD h:mm:ss')
+        console.log(date);
+        console.log( ip.address());
+        console.log(req.originalUrl);
         next();
-  }
-  );
+});
+
+
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+const commonMW = require ("../middlewares/commonMiddlewares");
+
+
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+    res.send("My midddleware testing api!")
 })
 
 
