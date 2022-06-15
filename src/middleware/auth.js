9b7d0cbd618e6f5287c70cf808fbbc041df235a4
aditisah <sditi.sah1@gmail.com>
2022-06-15 20:04:15 +0530

    assignment done 15th june

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,40 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
-
+const jwt = require("jsonwebtoken");
+    let userModel = require("../models/userModel")
+    
+const authenticate = async function(req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if(!token){
+      token = req.headers["x-auth-token"]
+    }
+      if(!token){
+        return res.send("token is manadatory");
+      }
+      let decodedToken = jwt.verify(token,"functionup-radon");
+      if(!decodedToken){
+        return res.send("Token is invalid")
+      } 
+      let userId = req.params.userId;
+      //let userData = req.body;
+      let user = await userModel.findById(userId);
+      //Return an error if no user with the given id exists in the db
+      if (!user) {
+        return res.send("No such user exists");
+      }
+      next();
+    }
+    
+const authorise =  function(req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if(!token){
+        token = req.headers["x-auth-token"]
+      }
+    let decodedToken = jwt.verify(token,"functionup-radon");
+        let userToBeModified = req.params.userId;
+        if(decodedToken.userId !== userToBeModified){
+            return res.send({status: false,msg:'you are not allowed to modify or delete other\'s data'})
+        }
     next()
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
